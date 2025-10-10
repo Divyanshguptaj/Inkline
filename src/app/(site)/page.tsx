26af783937bce0bdbb0e0af4ee14a25a5ef3d519
Divyanshguptaj <luckys510039@gmail.com>
2025-10-11 01:36:03 +0530

    added the login page and signup page with loader and zod types

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -114,9 +114,9 @@ const HomePage = () => {
           before:absolute
         "
         >
-          {[...Array(2)].map((arr) => (
+          {[...Array(2)].map((arr, index) => (
             <div
-              key={arr}
+              key={index}
               className="flex
                 flex-nowrap
                 animate-slide
